e14e84f29dc533bbde4fa53e6f53181728374642
msiemdaj <msiemdaj@outlook.com>
2026-04-11 14:05:24 +0200

    Swagger UI Documentation

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/docs', function () {
+    return response()->file(public_path('/swagger/index.html'));
 });
